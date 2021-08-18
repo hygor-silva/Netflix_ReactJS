e91a343cf43fbced2c1d88daa0d47a38573b3769
Hygor Silva <77420686+hygor-silva@users.noreply.github.com>
2021-08-17 21:50:07 -0300

    update date
    Inclusão até a data de lançamento

--- netflix-app/src/App.js
+++ netflix-app/src/App.js
@@ -7,12 +7,23 @@ import FeaturedMovie from './components/FeaturedMovie';
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
   useEffect(() => {
     const loadAll = async () => {
       //Pegando toda a lista
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      //Pegando o Featured
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random()*(originals[0].items.results.length -1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv' );
+
+      setFeaturedData(chosenInfo);
+
+      
     }
     loadAll();
 
@@ -20,8 +31,8 @@ export default () => {
 
   return(
     <div className="page">
+      {featuredData && <FeaturedMovie item={featuredData} />}
       
-
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title = {item.title} items={item.items} />
