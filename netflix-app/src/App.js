6bfaf8a954cb9d67fff69b2aa3cefe87180bb326
Hygor Silva <77420686+hygor-silva@users.noreply.github.com>
2021-08-19 12:32:39 -0300

    Final
    Todas as funcionalidades funcionando perfeitamente.

--- netflix-app/src/App.js
+++ netflix-app/src/App.js
@@ -67,6 +67,12 @@ export default () => {
         Dados pegos do site Themoviedb.org
 
       </footer>
+
+      {movieList.length <= 0 &&
+      <div className= "loading">
+          <img src= "https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt= "Carregando"></img>
+      </div>
+      }
     </div>
   );
 }
--- netflix-app/src/components/FeaturedMovie.js
+++ netflix-app/src/components/FeaturedMovie.js
@@ -9,6 +9,11 @@ export default ({item}) => {
         genres.push(item.genres[i].name);
     }
 
+    let description = item.overview;
+    if (description.length > 200){
+        description = description.substring(0, 200)+'...';
+    }
+
     return (
         <section className= "featured" style={{
             backgroundSize: 'cover',
@@ -25,7 +30,7 @@ export default ({item}) => {
                         <div className= "featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''} </div>
                     </div>
                     <div className= "featured--description">
-                        {item.overview}
+                        {description}
                     </div>
                     <div className= "featured--buttons">
                         <a href={`/watch/${item.id}`} className= "featured--watchbutton">▶ Assistir</a>
